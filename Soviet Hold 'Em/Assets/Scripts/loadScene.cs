cefe174ea50cdeedb359af7ae7e85a6f92317f63
Christine Lam <clam9623@bths.edu>
2021-12-19 17:55:07 -0500

    12/19/21 update b

--- Soviet Hold 'Em/Assets/Scripts/loadScene.cs
+++ Soviet Hold 'Em/Assets/Scripts/loadScene.cs
@@ -23,7 +23,7 @@ public class loadScene : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player") {
-            SceneManager.LoadScene("Level 2");
+            SceneManager.LoadScene("Dead Drop");
         }
     }
 }
